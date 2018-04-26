cb4fc24451348700aaf09d8b2b88dcab8ef63bab
Thomas1023 <thomas.steiner03@gmail.com>
2018-04-25 19:30:26 -0700

    fram

---
+++ src/MAin.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class MAin {
+public static void main(String[] args) {
+	JFrame JF = new JFrame();
+	JF.setVisible(true);
+	JF.setSize(600, 600);
+	JF.setTitle("JAvaaaaaaaaaaaaaaa");
+}
+}
